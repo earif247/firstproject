6fa74b3cc9ec952934d451bf1c9fe4f8c8364714
earif247 <ariful26426@gmail.com>
2019-11-01 21:05:30 +0600

    all are done

---
+++ index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>epic.A.photography</title>
+  </head>
+  <body>
+    <section style="position: relative;">
+      <div class="topmenu_area" style="position: fixed; background: transparent; z-index:9999;">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="log_area">
+                <img src="assets/image/LOGO.jpg">
+              </div>
+            </div>
+            <div class="col-md-8">
+
+              <div class="topmenu">
+                <a class="active" href="#home">Home</a>
+                
+                 <a href="#Pictures">Pictures</a>
+                 <a href="#blog">Blog</a>
+                 <a href="#contact">Contact</a>
+                 <a href="#about">About</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div style="position: absolute; top:0px;z-index:999;">
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="assets/image/slide-1.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="assets/image/slide-4.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="assets/image/slide-5.jpg" alt="Third slide">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <section>
+      
+      <div class="Pictures">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+  </body>
+</html>
